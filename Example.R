de6c38a3b38f6326411725181d25575fc5ead9d5
srinivasgurrala <srinivasgurrala@gmail.com>
2020-08-26 21:05:23 +0530

    Add files via upload

---
+++ Example.R
@@ -0,0 +1,43 @@
+
+1+1
+2+3*4
+exp(1)
+pi
+
+
+x <- 58
+y<-20  
+z<-100
+
+x*y*z
+
+(6*3)+ 5/2
+
+#Create a vector
+my_vect <- c(12,20,15,100)
+
+vect_x<-c(2,0,0,4)
+vect_y <- c(1,9,9,9)
+
+#add the vectors
+vect_add <- vect_x+vect_y
+
+#Mul vect_x with some scalar
+vect_x*5
+
+#Accessing the elements of the vector
+vect_x[1]
+vect_x[4]
+
+vect_x[2]<-2
+vect_x
+vect_char <- c('R','python','java',10,100.5)
+chaar_v <- c("a","b","c")
+
+
+cust<-data.frame(x=c(1,2,3),y=c("a","b","c"))
+
+cust[1,2,2]
+
+
+
